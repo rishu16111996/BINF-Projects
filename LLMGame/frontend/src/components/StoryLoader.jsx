57e09dcc640a77676fe736f446e9f3649103a3c5
Rishabh Narula <rishabhnarula@Rishabhs-MacBook-Pro.local>
2025-11-13 19:12:23 -0500

    LLM frontend complete

--- LLMGame/frontend/src/components/StoryLoader.jsx
+++ LLMGame/frontend/src/components/StoryLoader.jsx
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react';
-import {useParams, useNavigate} from 'react';
+import {useParams, useNavigate} from 'react-router-dom';
 import axios from 'axios';
 import LoadingStatus from './LoadingStatus';
  import StoryGame from './StoryGame';   
